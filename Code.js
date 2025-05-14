3038b6599c93cb2ae7d12da6fe17dae0a8912d6a
fcortesbio <fcortesbio@gmail.com>
2025-05-14 23:57:35 +0000

    testing clasp

--- Code.js
+++ Code.js
@@ -3,13 +3,11 @@ const DOMAIN_NAME = 'blond.com';
 const OWNER_NAME = 'Steve';
 const OWNER_EMAIL = 'steve.jobs@blond.com';
 const EID_LENGTH = 7;
-const favicon_url = "https://drive.google.com/drive/folders/1mzV4rCSd_LGTakCfGijVzZDeX9oHw_rZ"
 // Main function to serve the web app
 function doGet() {
     return HtmlService.createTemplateFromFile('index')
         .evaluate()
         .setTitle('Cal-PGT Prompt Generator Tool')
-        .setFaviconUrl(favicon_url)
         .addMetaTag('viewport', 'width=device-width, initial-scale=1')
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
