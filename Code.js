f4cf89eec3fd29a25393c6d92a0a341f3390fe7a
fcortesbio <fcortesbio@gmail.com>
2025-05-14 23:59:54 +0000

    testing clasp

--- Code.js
+++ Code.js
@@ -1,20 +1,19 @@
-const COMPANY = 'Blond';
-const DOMAIN_NAME = 'blond.com';
-const OWNER_NAME = 'Steve';
-const OWNER_EMAIL = 'steve.jobs@blond.com';
+const COMPANY = "Blond";
+const DOMAIN_NAME = "blond.com";
+const OWNER_NAME = "Steve";
+const OWNER_EMAIL = "steve.jobs@blond.com";
 const EID_LENGTH = 7;
+
 // Main function to serve the web app
 function doGet() {
-    return HtmlService.createTemplateFromFile('index')
-        .evaluate()
-        .setTitle('Cal-PGT Prompt Generator Tool')
-        .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  return HtmlService.createTemplateFromFile("index")
+    .evaluate()
+    .setTitle("Cal-PGT Prompt Generator Tool")
+    .addMetaTag("viewport", "width=device-width, initial-scale=1")
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 
 // Include HTML files
 function include(filename) {
-    return HtmlService.createHtmlOutputFromFile(filename).getContent();
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
-
-
