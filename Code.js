8b34d1a4f66f78d6e1e18d8caf32076f4b208a77
fcortesbio <fcortesbio@gmail.com>
2025-05-14 23:42:55 +0000

    testing clasp

--- Code.js
+++ Code.js
@@ -1,14 +1,15 @@
-const COMPANY = "Blond";
-const DOMAIN_NAME = "blond.com";
-const OWNER_NAME = "Steve";
-const OWNER_EMAIL = "steve.jobs@blond.com";
+const COMPANY = 'Blond';
+const DOMAIN_NAME = 'blond.com';
+const OWNER_NAME = 'Steve';
+const OWNER_EMAIL = 'steve.jobs@blond.com';
 const EID_LENGTH = 7;
 
 // Main function to serve the web app
 function doGet() {
     return HtmlService.createTemplateFromFile('index')
         .evaluate()
-        .setTitle('Cal Prompt Generator')
+        .setTitle('Cal-PGT Prompt Generator Tool')
+        .setFaviconUrl('https://www.google.com/s2/favicons?domain=google.com')
         .addMetaTag('viewport', 'width=device-width, initial-scale=1')
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
@@ -17,4 +18,5 @@ function doGet() {
 function include(filename) {
     return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
+
 
