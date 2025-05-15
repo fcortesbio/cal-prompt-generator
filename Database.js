b64f7326dd65d02bd650a6003d67d47ebd563929
Andres <fcortesbio@gmail.com>
2025-05-15 12:01:49 -0500

    refactor: switch users_data to a headered table; data retrieval assumes that user_data has a header row

--- Database.js
+++ Database.js
@@ -19,19 +19,23 @@ function getSheetByIndex(index) {
 }
 
 // Build a map of EID to user data
+// Assumes user_data sheet has a header row
 function buildEidMap() {
   const sheet = getSheet("user_data");
   const data = sheet.getDataRange().getValues();
 
   const eidMap = {};
-  data.forEach(row => {
+  
+  for (let i = 1; i < data.length; i++) { // Start from row 2 (index 1)
+    const row = data[i];
     eidMap[row[0]] = {
       agent_eid: row[0].toString(),
       agent_name: row[1],
       agent_division: row[2],
       agent_role: row[3],
     };
-  });
+  }
+
   return eidMap;
 }
 
